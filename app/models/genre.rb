a8f4467c57151024f5e41076735f4cbd96e76a78
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-18 16:33:39 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,7 +9,7 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    self.songs.artist.length
+    self.songs.artists.length
   end
 
   def all_artist_names
